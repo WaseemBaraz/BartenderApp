66e05b4e8cb6b82f9666564858b24d31bd29fd57
WaseemBaraz <waseem.baraz@gmail.com>
2016-09-04 10:25:33 -0400

    initial commit

---
+++ BartenderApp/Models/Drink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BartenderApp.Models
+{
+    public class Drink
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        
+    }
+
+    public class DrinkDbContext : DbContext
+    {
+        public DbSet<Drink> Drinks { get; set; }    
+
+           
+    }
+}
---
+++ BartenderApp/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BartenderApp.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Drink Drink { get; set; }
+    }
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
